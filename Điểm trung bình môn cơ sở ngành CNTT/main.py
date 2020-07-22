976c3e02218a0147811ad1ed45ba66a8174b72d3
hoangtuyenblogger <hoangtuyenblogger@gmail.com>
2020-07-22 11:06:27 +0700

    22/07/2020

--- Điểm trung bình môn cơ sở ngành CNTT/main.py
+++ Điểm trung bình môn cơ sở ngành CNTT/main.py
@@ -5,9 +5,7 @@ import numpy as np
 import random
 import math
 import sys
-
-
-
+import matplotlib.pyplot as plt
 
 
 # Load data tu CSV file
@@ -134,17 +132,17 @@ def main1():
 
 
 def size_training_test():
-    filename = 'hehe.csv'
-    splitRatio = 0.8
+    filename = 'data_khongco_tieude.csv'
+    splitRatio = 0.5
     dataset = load_csv(filename)
     training, test = split_data(dataset, splitRatio)
 
     print('Data size {0} \nTraining ={1} \nTest Size={2}'.format(len(dataset), len(training), len(test)))
-################################## phần phía trên e lụm trên mạng nên đừng quan tâm :3
+################################## phần phía trên e lụm trên mạng
 def main():
-    size_training_test()
     filename = 'data.csv'
     data = pd.read_csv(filename)
+
     ## load dữ liệu vào mảng
     SV = data["SV"]
     NMCNTT = data["NMCNTT"]
@@ -152,20 +150,35 @@ def main():
     KTLT = data["KTLT"]
     CTDLGT = data["CTDLGT"]
     MMT = data["MMT"]
+
+    print('Đối tượng thực hiện khảo sát chủ yếu là sinh viên năm', st.mode(SV))
     ####################################
+    print('################Tính điểm nhỏ nhất trong từng môn học#########################')
+    print('Điểm nhỏ nhất NMCNTT là: ',NMCNTT.min())
+    print('Điểm nhỏ nhất CSLT là: ', CSLT.min())
+    print('Điểm nhỏ nhất KTLT là: ', KTLT.min())
+    print('Điểm nhỏ nhất CTDLGT là: ', CTDLGT.min())
+    print('Điểm nhỏ nhất MMT là: ', MMT.min())
+    print('################Tính điểm lớn nhất trong từng môn học#########################')
+    print('Điểm lớn nhất NMCNTT là: ',NMCNTT.max())
+    print('Điểm lớn nhất CSLT là: ', CSLT.max())
+    print('Điểm lớn nhất KTLT là: ', KTLT.max())
+    print('Điểm lớn nhất CTDLGT là: ', CTDLGT.max())
+    print('Điểm lớn nhất MMT là: ', MMT.max())
+    #################################################################
+    print('################Tính điểm trung bình trong từng môn học#########################')
     ndigit = 1
-    print('Đối tượng thực hiện khảo sát chủ yếu là sinh viên năm',st.mode(SV))
     print('Điểm trung bình đối với từng môn học là:')
-    print('\nMean_NMCNTT= {0} \nMean_CSLT= {1} \nMean_KTLT= {2} \nMean_CTDLGT= {3} \nMean_MMT={4}'.format(round(NMCNTT.mean(),ndigit), round(CSLT.mean(),ndigit), round(KTLT.mean(),ndigit), round(CTDLGT.mean(),ndigit), round(MMT.mean(),ndigit)))
-
-
-
-
-
-
-
-
-
+    print('Mean_NMCNTT= {0} \nMean_CSLT= {1} \nMean_KTLT= {2} \nMean_CTDLGT= {3} \nMean_MMT={4}'.format(round(NMCNTT.mean(),ndigit), round(CSLT.mean(),ndigit), round(KTLT.mean(),ndigit), round(CTDLGT.mean(),ndigit), round(MMT.mean(),ndigit)))
+    print('#########################################')
+    print('Điểm xuất hiện nhiều nhất của NMCNTT là : ',st.mode(NMCNTT))
+    print('Điểm xuất hiện nhiều nhất của CSLT là : ', st.mode(CSLT))
+    print('Điểm xuất hiện nhiều nhất của KTLT là : ', st.mode(KTLT))
+    print('Điểm xuất hiện nhiều nhất của CTDLGT là : ', st.mode(CTDLGT))
+    print('Điểm xuất hiện nhiều nhất của MMT là : ', st.mode(MMT))
+    print('#########################################')
 
 if __name__ == '__main__':
     main()
+
+
